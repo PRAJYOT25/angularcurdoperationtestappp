5dfbf16e80556b82bb305ce7f1a1cba28ae7bcf1
pratibhaHankare <pratibha1hankare@gmail.com>
2020-10-03 23:45:36 +0530

    fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/angularcurdoperationtestappp'));
 app.get('/*', function(req,res) {
    
-    res.sendFile(path.join(__dirname+'../dist/angularcurdoperationtestappp/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/angularcurdoperationtestappp/index.html'));
 });
 
 
